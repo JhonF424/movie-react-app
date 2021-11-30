8b7cd73d3f78e4cf79fec6334387f4df699be5d3
Jhon Alejandro Franco Gómez <jhon@remotepcsolutions.com>
2021-11-29 23:38:31 -0500

    Realizado: Creación de componentes principales para la página, y para la lista de películas. Pendiente: Fetch al JSON de películas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+import Pelicula from './pelicula';
+import PageWrapper from './pageWrapper';
 
 function App() {
+  const [data, setData] = useState([]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+      <Pelicula titulo={data.titulo}>
+
+      </Pelicula>
+    </PageWrapper>
   );
 }
 
